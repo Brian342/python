b85ac9221560b21ccad9a9ecc4716292068b9157
Brian342 <migelbrian3@gmail.com>
2023-11-24 22:48:50 +0300

    introduction
    Signed-off-by: Brian342 <migelbrian3@gmail.com>

---
+++ demp.py
@@ -0,0 +1,17 @@
+# Write a program that accepts three numbers and
+# computes their sum, product and average.
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+
+Sum = num1 + num2 + num3
+average = Sum / 3
+product = num1 * num2 * num3
+
+print("The sum is->" + str(Sum))
+print("The average is->" + str(average))
+print("The product is->" + str(product))
+
+
+
+
