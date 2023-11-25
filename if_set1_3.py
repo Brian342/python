cddf834cc9045b23ddd9a8c964a94c9dbe1ac55b
Brian342 <migelbrian3@gmail.com>
2023-11-25 23:37:12 +0300

    positive/negative values
    Signed-off-by: Brian342 <migelbrian3@gmail.com>

--- if_set1_3.py
+++ if_set1_3.py
@@ -1,15 +1,12 @@
-# Write a program that prompts the user for two numbers and then computes them using the following rules.
-# If the first number is greater than the second one, the second number is subtracted from the first one.
-# If the second number is greater than the first one, the first number is divided by the second one.
-# Otherwise, the two numbers are added.
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+# Write a program to read a value from the keyboard and output the phrase NEGATIVE if the number is negative,
+# POSITIVE if the number is positive or ZERO otherwise.
+value = int(input("Enter a value from the keyboard: "))
 
-if num1 > num2:
-    print("num1 - num2 = " + str(num1-num2))
-elif num1 < num2:
-    print("num2 // num1 = " + str(num1 / num2))
+if value < 0:
+    print("NEGATIVE")
+elif value > 0:
+    print("POSITIVE")
 else:
-    print("Num1 + num2 = " + str(num1 + num2))
+    print("ZERO")
 
 
