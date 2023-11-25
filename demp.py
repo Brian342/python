e1b04b5aed85c3c1aedb2b4e2150d46d4a99c471
Brian342 <migelbrian3@gmail.com>
2023-11-25 14:05:26 +0300

    for loop
    Signed-off-by: Brian342 <migelbrian3@gmail.com>

--- demp.py
+++ demp.py
@@ -1,12 +1,13 @@
 # Write a program that prints God bless Kenya. 25 times on the screen using: -
-# do…while loop.
 # while… loop
 # for… loop.
-# while loop
-name = 0
-while name <= 25:
+# for loop
+for i in range(25):
     print("God Bless Kenya")
-    name += 1
+
+
+
+
 
 
 
