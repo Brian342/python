1c8b455ccb2a97cc98e1fd41572e87eb88c615cf
Brian342 <migelbrian3@gmail.com>
2023-11-25 09:11:27 +0300

    calculating speed given distance in metres and time in minutes to KM/H
    Signed-off-by: Brian342 <migelbrian3@gmail.com>

--- demp.py
+++ demp.py
@@ -1,9 +1,15 @@
-# Write a program that accepts temperature in degrees Celsius and converts and outputs it in degrees Fahrenheit.
+# Write a program that accepts distance traveled in metres and the time taken in
+# minutes and then outputs the speed in kilometers per hour (km/h). E.g. if someone enters a distance (in metres)
+# of 500 and a time (in minutes) of 15 the output for speed should be 2 km/h.
+import math
 
-temp = int(input("Enter the Temperature in degree celsius->"))
-convert_degree_to_fahrenheit = (temp * (0.9/0.5) + 32)
+distance = int(input("Enter distance in metres->"))
+time = int(input("Enter time in minutes->"))
 
-print("The temperature is->" + str(temp))
-print("The temperature in degree fahrenheit is->" + str(convert_degree_to_fahrenheit))
+convert_distance_to_kilometres = distance / 1000
+convert_minutes_to_hour = time / 60
 
+speed = math.floor(convert_distance_to_kilometres / convert_minutes_to_hour)
+
+print("The speed is->" + str(speed) + " Km/h")
 
