06ad80c2b4c295a73edd2179df00beea70f64dea
Brian342 <migelbrian3@gmail.com>
2023-11-25 13:34:05 +0300

    square,sqrRoot,cube,cubeRoot
    Signed-off-by: Brian342 <migelbrian3@gmail.com>

--- demp.py
+++ demp.py
@@ -1,13 +1,16 @@
-# Write a program that accepts the age of a person,
-# if the person is 18 and above, it should print You can vote.
-# otherwise it should print You are too young to vote.
+# Write a program that accepts a number and then outputs its square, cube, square root and cube root.
+import math
+number = int(input("Enter a number->"))
+square = math.pow(number, 2)
+cube = math.pow(number, 3)
+sqrt = math.sqrt(number)
+cubeRoot = math.cbrt(number)
 
-age = int(input("Enter your age->"))
+output = (f'the square is->{math.floor(square)}, The cube is->{math.floor(cube)}, The sqrt is->{math.floor(sqrt)}'
+          f', The cube-root is->{cubeRoot}')
 
-if age >= 18:
-    print("You can vote!")
-else:
-    print("You are too young to vote!")
+print(output)
+
 
 
 
