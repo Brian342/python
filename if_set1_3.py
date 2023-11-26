e52bd53943adbf098704da7e76b283b474fcd40d
Brian342 <migelbrian3@gmail.com>
2023-11-26 11:08:52 +0300

    positive/negative values
    Signed-off-by: Brian342 <migelbrian3@gmail.com>

--- if_set1_3.py
+++ if_set1_3.py
@@ -1,12 +1,13 @@
-# Write a program to read a value from the keyboard and output the phrase NEGATIVE if the number is negative,
-# POSITIVE if the number is positive or ZERO otherwise.
-value = int(input("Enter a value from the keyboard: "))
+# Write a program that accepts two numbers and divides the bigger number by the smaller number and displays the results.
+# The program should display an error message (and not perform the calculation) if the smaller number is zero.
+num1 = int(input("Enter number1: "))
+num2 = int(input("Enter number2: "))
 
-if value < 0:
-    print("NEGATIVE")
-elif value > 0:
-    print("POSITIVE")
-else:
-    print("ZERO")
+if num1 > num2:
+    print("num1 / num2: " + str(num1 / num2))
+elif num1 < num2:
+    print("num2 / num1: " + str(num2 / num1))
+elif num1 <= 0 or num2 <= 0:
+    print("Error can't divide by a zero value!!")
 
 
